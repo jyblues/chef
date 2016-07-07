9726cb81a0a35cea5367721a4831eb371745d7c5
jyblues <jyblues@gmail.com>
2016-07-08 02:28:23 +0900

    Update install_ruby_and_chef.sh

--- install_ruby_and_chef.sh
+++ install_ruby_and_chef.sh
@@ -3,28 +3,4 @@ cd ~
 git clone https://github.com/jyblues/chef.git
 sudo chmod +x ./chef/gameserver.sh
 sudo chmod +x ./chef/test.sh
-
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /usr/local/rvm/scripts/rvm
-
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
-source ~/.bash_profile
-
-rbenv install -l
-rbenv install -v 2.2.3
-rbenv rehash
-
-rbenv global 2.2.3
-
-rbenv versions
-
-curl -L http://www.opscode.com/chef/install.sh | sudo bash
-chef-solo -v
-
-gem install chef
-
-gem install knife-solo
+sudo chmod +x ./chef/install_ruby_and_chef.sh
